df858a12f432c5f254f71221de364f2a6a91a0ca
Duncan Rout <65206033+duncanrout@users.noreply.github.com>
2022-02-10 13:58:17 -0600

    Update

--- routes/form.js
+++ routes/form.js
@@ -1,5 +1,17 @@
 const express = require("express")
 const router = express.Router()
+//reads in the json file as a string
+const fs = require('fs')
+fs.readFile('./businessdata.json', 'utf8', (err, jsonString) => {
+    if (err) {
+        console.log("File read failed:", err)
+        return
+    }
+    //console.log('File data:', jsonString)
+    //parses the string to an object... example of how to access: json[1]['B']
+    var json = JSON.parse(jsonString)
+    console.log(json[1]['B'])
+})
 
 router.use(logger)
 
@@ -16,12 +28,16 @@ router.get("/companyQuery", (req, res) => {
 router.post("/", (req, res) => {
   const isValid = true
   if (isValid) {
-    console.log(req.body.firstName)
-    form.push({ firstName: req.body.firstName })
+    //console.log(req.body.searchValue)
+    form.push({ searchValue: req.body.searchValue })
+    form.push({ city: 1 })
+    form.push({ phone: 1 })
+    form.push({ website: 1 })
+    console.log(form)
     res.redirect(`/form/${form.length - 1}`)
   } else {
     console.log("Error")
-    res.render("companyQuery", { firstName: req.body.firstName })
+    res.render("companyQuery", { searchValue: req.body.searchValue })
   }
 })
 
@@ -29,16 +45,17 @@ router
   .route("/:id")
   .get((req, res) => {
     console.log(req.user)
-    res.send(`Get User With ID ${req.params.id}`)
+    //res.send(`Get ID ${req.params.id}`)
+    res.send(`Form: ${form[1]}`)
   })
   .put((req, res) => {
-    res.send(`Update User With ID ${req.params.id}`)
+    res.send(`Update ID ${req.params.id}`)
   })
   .delete((req, res) => {
-    res.send(`Delete User With ID ${req.params.id}`)
+    res.send(`Delete ID ${req.params.id}`)
   })
 
-const form = [{ name: "Kyle" }, { name: "Sally" }]
+const form = []
 router.param("id", (req, res, next, id) => {
   req.user = form[id]
   next()
