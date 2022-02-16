ce06d91da0307ff559cfb6074e946f110fdd0b7a
Duncan Rout <65206033+duncanrout@users.noreply.github.com>
2022-02-15 20:39:38 -0600

    Update

--- routes/form.js
+++ routes/form.js
@@ -1,20 +1,22 @@
 const express = require("express")
 const router = express.Router()
+
 //reads in the json file as a string
 const fs = require('fs')
-fs.readFile('./businessdata.json', 'utf8', (err, jsonString) => {
+const data = fs.readFileSync('./businessdata.json', 'utf8', (err, jsonString) => {
     if (err) {
         console.log("File read failed:", err)
         return
     }
     //console.log('File data:', jsonString)
-    //parses the string to an object... example of how to access: json[1]['B']
-    var json = JSON.parse(jsonString)
-    console.log(json[1]['B'])
 })
 
-router.use(logger)
+//parses the string to an object... example of how to access: json[1]['B']
+var json = JSON.parse(data)
+console.log(json[1]['B'])
 
+
+router.use(logger)
 router.get("/", (req, res) => {
   //how we are to pull data from query
   console.log(req.query.name)
@@ -41,12 +43,21 @@ router.post("/", (req, res) => {
   }
 })
 
+const form = []
+
+router.param("id", (req, res, next, id) => {
+  req.user = form[id]
+  next()
+})
+
 router
   .route("/:id")
   .get((req, res) => {
     console.log(req.user)
     //res.send(`Get ID ${req.params.id}`)
-    res.send(`Form: ${form[1]}`)
+    const string = form[1]
+    console.log(string)
+    res.send(`Form: ${JSON.stringify(form[0])}`)
   })
   .put((req, res) => {
     res.send(`Update ID ${req.params.id}`)
@@ -54,12 +65,6 @@ router
   .delete((req, res) => {
     res.send(`Delete ID ${req.params.id}`)
   })
-
-const form = []
-router.param("id", (req, res, next, id) => {
-  req.user = form[id]
-  next()
-})
 
 function logger(req, res, next) {
   console.log(req.originalUrl)
