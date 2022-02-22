f81aa6fb26d756f310c43dbf5289650376c03f1e
Duncan Rout <65206033+duncanrout@users.noreply.github.com>
2022-02-22 14:15:19 -0600

    Update

--- routes/form.js
+++ routes/form.js
@@ -4,54 +4,28 @@ const data = require('./data');
 //reads in the json file as a string
 
 router.use(logger)
-/*router.get("/", (req, res) => {
-  //how we are to pull data from query
-  //console.log(req.query.name)
-  res.send("temp")
-})*/
 
 router.get("/companyQuery", (req, res) => {
-  res.render("companyQuery")
+  res.render("companyQuery", {qs : req.query})
 })
 
-router.post('/', (req, res, next) => {
-  //const filters =   req.query
-  filters = { A: '01008118914' }
-  //example... localhost:3000/?A=uid&B=name
-  const filteredUsers = data.filter(user => {
+router.get("/companyQuery/results", (req, res, next) => {
+  //Temporary Soln for formatting of data.js
+  console.log(req.query.searchValue)
+  const filters = { B: req.query.searchValue }
+  const filtered = data.filter(business => {
+    //business is a json object of each business cluster
   	let isValid = true
     for (key in filters) {
-    	console.log(key, user[key], filters[key])
-    	isValid = isValid && user[key] == filters[key]
+    	//console.log(key, business[key], filters[key])
+    	//Looks at specific keyl.. isValid = isValid && business[key] == filters[key]
+      isValid = isValid && business[key].includes(filters[key])
+      console.log(isValid)
   	}
   	return isValid
   });
-  res.send(filteredUsers)
+  res.send(filtered)
 });
-
-/*router.post("/", (req, res) => {
-  const isValid = true
-  if (isValid) {
-    //console.log(req.body.searchValue)
-    form.push({ searchValue: req.body.searchValue })
-    form.push({ city: 1 })
-    form.push({ phone: 1 })
-    form.push({ website: 1 })
-    console.log(form)
-    res.redirect(`/form/${form.length - 1}`)
-  } else {
-    console.log("Error")
-    res.render("companyQuery", { searchValue: req.body.searchValue })
-  }
-})
-
-
-const form = []
-router.param("id", (req, res, next, id) => {
-  req.user = form[id]
-  next()
-})
-*/
 
 function logger(req, res, next) {
   console.log(req.originalUrl)
